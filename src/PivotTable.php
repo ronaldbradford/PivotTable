73e961bf8c441ad17ed2c9bdfc75512380fb2185
Ronald Bradford <me@ronaldbradford.com>
2015-03-23 21:39:15 -0400

    Initial placeholder class

---
+++ src/PivotTable.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace PivotTable;
+
+class PivotTable
+{
+
+    public function __construct() {
+    }
+
+    public function generate() {
+    }
+
+    public function render() {
+    }
+}
