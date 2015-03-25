a235136ae17a1e45a66fe9b1035d0908e5da526d
Ronald Bradford <me@ronaldbradford.com>
2015-03-25 12:22:01 -0400

    Refactor SalesExample Test to use Database class and pivot table abstracted pivot column results

--- tests/SalesExample.php
+++ tests/SalesExample.php
@@ -2,6 +2,7 @@
 namespace PivotTable;
 
 require (__DIR__ . '/../src/PivotTable.php');
+require (__DIR__ . '/../src/Database.php');
 require (__DIR__ . '/../src/Util.php');
 
 class SalesExample
@@ -18,18 +19,15 @@ class SalesExample
         $database = 'pivot_table';
         
         try {
-            $con = new \mysqli($host, $user, $password, $database);
-            if ($con->connect_errno) {
-                print 'Unable to obtain a database connection. '.mysqli_connect_error();
-                return;
-            }
+            $con = Database::getConnection($host, $user, $password, $database);
         } catch (Exception $e) {
             print 'Exception: Unable to obtain a database connection ' .$e->geteMessage();
             return;
         }
 
 
-        $columns_stmt = $con->prepare('SELECT DISTINCT sale_date FROM sale ORDER BY sale_date');
+        //$columns_stmt = $con->prepare('SELECT DISTINCT sale_date FROM sale ORDER BY sale_date');
+        $columns_data = $con->select('SELECT DISTINCT sale_date FROM sale ORDER BY sale_date', array('sale_date'));
         $data_sql  = 'SELECT product_type, sale_date, SUM(amt) AS sum_amt, SUM(qty) AS sum_qty ';
         $data_sql .= 'FROM   sale ';
         $data_sql .= 'GROUP BY product_type, sale_date ';
@@ -40,7 +38,7 @@ class SalesExample
             $pivot_column = array('sale_date' => 'Sale Date');
             $summation_columns = array('sum_qty' => 'Qty','sum_amt' => 'Sales');
             //$summation_columns = array('sum_qty' => 'Qty');
-            $a = $pt->summarize($con, $columns_stmt, $data_stmt, $pivot_row, $pivot_column, $summation_columns);
+            $a = $pt->summarize($con, $data_stmt, $columns_data, $pivot_row, $pivot_column, $summation_columns);
 
             $decorator = array("table"=> "table table-condensed table-striped table-bordered table-text-center");
             print '<h2>Product Type by Date Example <small>'.Util::downloadLink('demo1', $a).'</small></h2>';
@@ -48,10 +46,11 @@ class SalesExample
         } catch (Exception $e) {
             print $e->geteMessage();
             return;
-        } finally {
-            if ($con) {
-                mysqli_close($con);
-            }
+        }
+        finally {
+        if ($con) {
+            $con->close();
+        }
         }
 
 
