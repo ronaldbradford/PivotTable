27289ec5e35edeec77b8150253a6db7e53fc51fb
Ronald Bradford <me@ronaldbradford.com>
2015-03-25 13:25:19 -0400

    Updated test to use refactored Pivot Table functionality

--- tests/SalesExample.php
+++ tests/SalesExample.php
@@ -26,21 +26,28 @@ class SalesExample
         }
 
 
-        //$columns_stmt = $con->prepare('SELECT DISTINCT sale_date FROM sale ORDER BY sale_date');
-        $columns_data = $con->select('SELECT DISTINCT sale_date FROM sale ORDER BY sale_date', array('sale_date'));
+        $pivot_column_values_sql = 'SELECT DISTINCT sale_date FROM sale ORDER BY sale_date';
+        $pivot_column_values = $con->select($pivot_column_values_sql, array('sale_date'));
+
         $data_sql  = 'SELECT product_type, sale_date, SUM(amt) AS sum_amt, SUM(qty) AS sum_qty ';
         $data_sql .= 'FROM   sale ';
         $data_sql .= 'GROUP BY product_type, sale_date ';
         $data_sql .= 'ORDER BY product_type, sale_date ';
-        $data_stmt = $con->prepare($data_sql);
+
+        $results_data = $con->select($data_sql, array('product_type', 'sale_date', 'sum_amt', 'sum_qty'));
+
         try {
             $pivot_row = array('product_type' => 'Product Type');
             $pivot_column = array('sale_date' => 'Sale Date');
             $summation_columns = array('sum_qty' => 'Qty','sum_amt' => 'Sales');
             //$summation_columns = array('sum_qty' => 'Qty');
-            $a = $pt->summarize($con, $data_stmt, $columns_data, $pivot_row, $pivot_column, $summation_columns);
+            $a = $pt->summarize($results_data, $pivot_column_values, $pivot_row, $pivot_column, $summation_columns);
 
-            $decorator = array("table"=> "table table-condensed table-striped table-bordered table-text-center");
+            $decorator = array(
+              'table' => 'table table-condensed table-striped table-bordered table-text-center',
+              'total_row' => 'info',
+              'pivot_row' => 'success aleft'
+            );
             print '<h2>Product Type by Date Example <small>'.Util::downloadLink('demo1', $a).'</small></h2>';
             $o = $pt->render($a, $decorator, count($summation_columns));
         } catch (Exception $e) {
